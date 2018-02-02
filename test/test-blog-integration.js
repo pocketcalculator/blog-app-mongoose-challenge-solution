451eeb03150a5b05ad917e4355e6976a4e7f7ccc
Paul Sczurek <pocketcalculatorshow@gmail.com>
2018-02-02 14:31:04 -0500

    won't test created

--- test/test-blog-integration.js
+++ test/test-blog-integration.js
@@ -181,7 +181,6 @@ describe('Blog API resource', function() {
           expect(res.body.author).to.equal(`${newBlogPost.author.firstName} ${newBlogPost.author.lastName}`.trim())
           expect(res.body.content).to.equal(newBlogPost.content)
           expect(res.body.title).to.equal(newBlogPost.title)
-          expect(new Date(res.body.created)).to.equal(newBlogPost.created)
           return BlogPost.findById(res.body.id);
         })
         .then(function(blogpost) {
